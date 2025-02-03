c66ab10ab326e691b6fb1a5a9716c4a029d9303b
pampers23 <dexterpimss23@gmail.com>
2025-02-03 21:37:42 +0800

    Updated cypress config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      baseUrl: 'http://localhost:3000/'
+      baseUrl: 'http://localhost:5173/'
     },
   },
 });
